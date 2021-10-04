ae40f1a62fc45d7cac515c52fd1fad87b8c84065
jcardus <asklocation.net@gmail.com>
2021-10-04 01:28:53 -0300

    remove log

--- addons/auth_oauth/controllers/main.py
+++ addons/auth_oauth/controllers/main.py
@@ -18,6 +18,7 @@ from odoo import registry as registry_get
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome as Home
 from odoo.addons.web.controllers.main import db_monodb, ensure_db, set_cookie_and_redirect, login_and_redirect
 
+
 _logger = logging.getLogger(__name__)
 
 
