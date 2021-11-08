a158ac6c1ea67704d75085fbad01d936bf9a505a
jpenas <jppenas@gmail.com>
2021-11-08 18:42:43 +0000

    Add custom module

---
+++ addons/fleet_fleetmap/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Fleetmap',
+    'version' : '1.0',
+    'summary' : 'Fleetmap Management'
+    'sequence': 185,
+    'category': 'Human Resources/Fleet',
+    'website' : '',
+    'depends': [
+        'fleet'
+    ],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'auto_install': True
+}
