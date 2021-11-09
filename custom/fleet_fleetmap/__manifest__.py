06d278d64790eba5e552c58ea2edb95d21c32556
jpenas <jppenas@gmail.com>
2021-11-09 13:21:38 +0000

    Add custom module

--- custom/fleet_fleetmap/__manifest__.py
+++ custom/fleet_fleetmap/__manifest__.py
@@ -1,9 +1,11 @@
 {
     'name' : 'Fleetmap',
     'version' : '0.0.1',
+    'sequence': 185,
     'summary' : 'Fleetmap Management',
-    'category': 'Human Resources',
+    'category': 'Human Resources/Fleet',
     'website' : '',
+    'depends': ['fleet'],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
