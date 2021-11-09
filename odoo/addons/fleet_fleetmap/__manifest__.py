8d2f86248b9734d31858cddab1d6cc089b21fab9
jpenas <jppenas@gmail.com>
2021-11-09 12:45:30 +0000

    Add custom module

---
+++ odoo/addons/fleet_fleetmap/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Fleetmap',
+    'version' : '0.0.1',
+    'summary' : 'Fleetmap Management'
+    'sequence': 0,
+    'category': 'Hidden/Tests',
+    'website' : '',
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'auto_install': False
+}
