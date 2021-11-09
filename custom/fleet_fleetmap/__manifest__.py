94183e4d44eed448aba487d907864b3fadcd7053
jpenas <jppenas@gmail.com>
2021-11-09 13:12:10 +0000

    Add custom module

--- custom/fleet_fleetmap/__manifest__.py
+++ custom/fleet_fleetmap/__manifest__.py
@@ -1,9 +1,8 @@
 {
     'name' : 'Fleetmap',
     'version' : '0.0.1',
-    'summary' : 'Fleetmap Management'
-    'sequence': 0,
-    'category': 'Hidden/Tests',
+    'summary' : 'Fleetmap Management',
+    'category': 'Human Resources',
     'website' : '',
     'installable': True,
     'application': True,
