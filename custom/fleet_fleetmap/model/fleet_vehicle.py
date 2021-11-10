6c36db4fba1e3f668394cd50b00a59cf1a7e5f1a
jpenas <jppenas@gmail.com>
2021-11-10 14:31:58 +0000

    try to extend fleet_vehicle

--- custom/fleet_fleetmap/model/fleet_vehicle.py
+++ custom/fleet_fleetmap/model/fleet_vehicle.py
@@ -1,7 +1,12 @@
-from odoo import fields, models
+
+from odoo import api, fields, models
+
 
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
-    fleetmap_name = fields.Char('Name')
-    fleetmap_id = fields.Integer('Fleetmap ID')
+    mobility_card_test = fields.Char(compute='_compute_mobility_card_test', store=True)
+
+    def _compute_mobility_card_test(self):
+        for vehicle in self:
+            vehicle.mobility_card_test = '1'
