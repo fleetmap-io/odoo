e333bf4268b241cec5d52c3572861cf9de02b18a
jpenas <jppenas@gmail.com>
2021-11-10 20:43:39 +0000

    add external dependencies

--- custom/fleet_fleetmap/__manifest__.py
+++ custom/fleet_fleetmap/__manifest__.py
@@ -7,7 +7,7 @@
     'website' : '',
     'depends': ['fleet'],
     'data': ['views/fleet_vehicle_views.xml'],
-    "external_dependencies": {"python" : ["pytraccar"]}
+    'external_dependencies': {'python' : ['pytraccar']},
     'auto_install': True,
     'license': 'LGPL-3'
 }
