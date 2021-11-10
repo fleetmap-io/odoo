485b64fca1b7cbc3f25f51ef2c88832578f9f6f8
jpenas <jppenas@gmail.com>
2021-11-10 16:06:55 +0000

    try to extend fleet_vehicle_odometer

--- custom/fleet_fleetmap/models/fleet_vehicle.py
+++ custom/fleet_fleetmap/models/fleet_vehicle.py
@@ -5,3 +5,18 @@ class FleetVehicle(models.Model):
 
     fleetmap_name = fields.Char('Name')
     fleetmap_id = fields.Integer('Fleetmap ID')
+
+
+class FleetVehicleOdometer(models.Model):
+    _inherit = 'fleet.vehicle.odometer'
+
+    fleetmap_test = fields.Char('test')
+    user_id = fields.Many2one('res.users', 'Responsible', default=lambda self: self.env.user, index=True)
+
+    @api.model
+    def create(self, vals):
+        vals['fleetmap_test'] = 'teste'
+        result = super(FleetVehicleOdometer, self).create(vals)
+
+        return result
+
